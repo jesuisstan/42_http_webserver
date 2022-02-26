5115fdb0b94a401075ddddb8a13eb84ef628504d
Taras Levko <arheyy@gmail.com>
2022-02-26 20:28:06 +0500

    return server folder

---
+++ src/server/Server.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+# include "webserv.hpp"
+
+class RequestParser;
+
+class Server {
+private:
+	int 			_listenSocket;
+	sockaddr_in		_servAddr;
+	struct pollfd	_fds[BACKLOG];
+	int				_timeout;
+	int				_numberFds;
+
+public:
+	Server();
+	~Server();
+	Server(const Server &other);
+	Server	&operator = (const Server &other);
+	void	setTimeout(int timeout);
+	int		getListenSocket(void) const;
+	int		getTimeout(void) const;
+	int		getNumberFds(void) const;
+	void	initiate(const char *ipAddr, int port);
+	void	runServer(int timeout);
+	void	closeConnections(void);
+
+    static bool findReqEnd(std::string request_buffer, size_t request_len);
+};
+
+void	interruptHandler(int sig_int);
